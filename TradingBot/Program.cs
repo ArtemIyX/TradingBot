57fd18f6eae0039e21a2580cc6fac9032e8e4954
Wellsaik <1play2fun123@gmail.com>
2023-03-18 12:02:30 +0200

    Добавьте файлы проекта.

---
+++ TradingBot/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using TradingBot.Services;
+
+namespace TradingBot;
+
+public static class Programm
+{
+    // Define a static async method named Main, which is the entry point for the application.
+    public static async Task Main(string[] args)
+    {
+        IHost host = AppStartup();
+        Application app = ActivatorUtilities.CreateInstance<Application>(host.Services);
+
+        app.Start();
+    }
+
+    // Define a static method named ConfigSetup to set up the configuration for the application.
+    public static void ConfigSetup(IConfigurationBuilder builder)
+    {
+        // Set the base path for the configuration file to the current directory.
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            // Add the appsettings.json file to the configuration, require the file to exist, and reload on change.
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            // Add environment variables to the configuration.
+            .AddEnvironmentVariables();
+    }
+
+    // Define a static method named AppStartup to initialize the application and return an instance of the host.
+    static IHost AppStartup()
+    {
+        // Create a new configuration builder.
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+
+        // Call the ConfigSetup method to configure the builder.
+        ConfigSetup(builder);
+
+        // Create a new logger using the configuration built by the ConfigurationBuilder.
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        // Create a new instance of the host with the default settings, configure services, add Serilog, and build the host.
+        IHost host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<Application>();
+        }).UseSerilog()
+        .Build();
+
+        // Return the host instance.
+        return host;
+    }
+}
---
+++ TradingBot/Services/Application.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingBot.Services
+{
+    internal class Application
+    {
+        private readonly ILogger<Application> _logger;
+        private readonly IConfiguration _config;
+        public Application(ILogger<Application> logger, IConfiguration config = null)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public void Start()
+        {
+            _logger.LogInformation("Hello world!");
+        }
+    }
+}
